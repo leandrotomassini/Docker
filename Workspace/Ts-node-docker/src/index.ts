f5b81d8d8b1cff2de524403503ecb4c5e5e37891
Leandro C. Tomassini <leandroctomassini@gmail.com>
2022-05-12 16:15:00 -0300

    Make file y producción

--- Workspace/Ts-node-docker/src/index.ts
+++ Workspace/Ts-node-docker/src/index.ts
@@ -3,6 +3,6 @@ import express from 'express';
 const app = express();
 
 app.listen(4000, () => {
-    console.log(`Mundla rrrerererereerrunning on port 40020 funciona.`);
+    console.log(`Hola mundo on port 40020 funciona.`);
 });
 
